cd2546906d8da3a4057804bd3191c3afc9c74665
akbar2020 <shaikakbar250281@yahoo.co.in>
2021-12-15 20:46:06 +0300

    validations5

--- src/app/pages/admission-form/father-detail/father-detail.component.ts
+++ src/app/pages/admission-form/father-detail/father-detail.component.ts
@@ -42,8 +42,8 @@ export class AdmissionFormFatherDetailComponent implements OnInit {
   employed: number;
   jobtitle: number;
   jobsector: number;
-  mobilenumber: number;
-  homenumber: number;
+  mobilenumber: string;
+  homenumber: string;
   emailaddress: string;
 
 
